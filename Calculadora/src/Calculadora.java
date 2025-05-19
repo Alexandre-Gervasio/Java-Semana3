67ed4667c2ff79ceff0b8c06a4523bd44ea277f2
AleGerva <alexandrework010@gmail.com>
2025-05-19 18:25:29 -0300

    Enviando Java-Semana3 para o GitHub

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemática entre números
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe para entender como utilizá-la corretamente.
+ * 
+ * @author Alexandre Gervasio
+ * @version 1.0
+ * @since 24/04/2025
+ */
+
+ public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros.
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+ }
---
+++ Calculadora/src/Comentarios.java
@@ -0,0 +1,18 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário de uma linha
+        /*
+         * Olá, eu sou um comentário de múltiplas linhas
+         * e posso ser usado para descrever partes do código
+         * ou para deixar anotações.
+         */
+        /**
+         * Olá, eu sou um comentário de documentação
+         * e sou utilizado para gerar a documentação do código.
+         * 
+         * @author Seu Nome
+         * @version 1.0
+         */
+        System.out.println("Hello, World!"); // Imprime uma mensagem no console
+    }
+}
